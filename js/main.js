3a7cfb97323451654d00123b2bb06ea1ba307734
ddmilo <ddmilo1029@gmail.com>
2017-03-08 12:41:01 -0500

    started JS

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var tenValue = 10;
+var jack = tenValue;
+var queen = tenValue;
+var king = tenValue;
+var faceCards = [jack, queen, king];
+
+
+
+var tenVal = [10];
+var singleValue = [2, 3, 4, 5, 6, 7, 8, 9];
+var aceValue = [1, 11];
+
+var cardValue = singleValue.concat(tenVal, aceValue, faceCards);
+
+console.log(cardValues);
